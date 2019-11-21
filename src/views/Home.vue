3c3f4366c9a13da1c66164e63c459705f7597a88
VedranN98 <mister225415@gmail.com>
2019-11-21 16:16:59 +0100

    initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="home">
+ 
+    <h1>Welcome to my page.</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum ipsam harum cumque dolorem. Qui quae, consequuntur porro harum atque quis alias tempore consectetur at libero praesentium animi ullam dignissimos aut.</p>
+  </div>
+  
+</template>
+
